5a0b0a77e14845f81dba97374bd1826edf8dcc4a
mitchbrunke <65658409+mitchbrunke@users.noreply.github.com>
2021-05-27 16:28:48 +1000

    Hero

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,17 @@ export default function Home() {
         <h4>Make the impossible possible at the</h4>
         <h1>Mary Poppins Festival</h1>
       </div>
+
+      <div className="info">
+        <h3>Welcome to the 13th Annual Mary Poppins Festival</h3>
+        <p>
+          This year’s Festival presents a four day program of storytelling for
+          all ages through all art forms. From music concerts backstage and
+          onstage at the Brolga to pop up performances in the Bond Store
+          Basement.
+        </p>
+      </div>
+
       <AnimatedMP />
     </div>
   );
