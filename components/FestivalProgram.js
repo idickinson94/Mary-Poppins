63a7653f9163cb9ec79101c16af1ba8c7442b64c
mitchbrunke <65658409+mitchbrunke@users.noreply.github.com>
2021-06-28 09:18:44 +1000

    Program Changes

--- components/FestivalProgram.js
+++ components/FestivalProgram.js
@@ -9,17 +9,32 @@ const FestivalProgram = () => {
       <div className={styles.progCard}>
         <div className={styles.img}></div>
         <div className={styles.text}>
-          <h4>Coming soon</h4>
+          <h4>The Mary Poppins Festival program is here! ☂️🙌</h4>
           <p>
-            Stay tuned for the program release in the coming weeks. For now, sit
-            back, save the date and start planning!
+            Set over four days, festivalgoers can expect a stellar lineup of
+            performances, entertainment and activities for the whole family. 👨‍👩‍👧‍👦
+          </p>
+
+          <p>
+            Day 1 (July 1) begins at the renowned Maryborough Markets and
+            includes a Basket Weaving Workshop at Gatakers Artspace and ticketed
+            tours at Story Bank and Bond Store. 🧶 Day 2 (July 2) includes
+            ticketed tours at the unique Story Bank and Bond Store, along with
+            entry into Gatakers Artspace. 🎨 Day 3 (July 3) includes a Recycled
+            Materials Workshop at Gatakers Artspace, an incredible ticketed
+            performance 'Charlotte's Web' at the Brolga Theatre. Again tours are
+            available at Story Bank and the Bond Store. 🕸️🎭 Day 4 (July 4) ✨
+            DAY IN THE PARK ✨ highlights include the Chimney Sweep and Nanny
+            Race Challenge, Best Dressed, Picnic in the Park, various Book
+            Times, Trivia, Magic Workshop, art and music performances and much
+            more!
           </p>
         </div>
       </div>
 
       <Link href="/program">
         <a>
-          <button>VIEW PROGRAM</button>
+          <button>VIEW FULL PROGRAM</button>
         </a>
       </Link>
     </div>
