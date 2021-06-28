3235d57530578e9082e8049a0f91ed42c64630e6
mitchbrunke <65658409+mitchbrunke@users.noreply.github.com>
2021-06-29 08:57:12 +1000

    covid update

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,18 @@ export default function Home() {
       </div>
 
       <div className={styles.info}>
-        <h3>Welcome to the 13th Annual Mary Poppins Festival!</h3>
+        <h3>
+          Mary Poppins Festival 2021 will be proceeding, organisers are
+          monitoring the situation closely.
+        </h3>
+
+        <p>
+          We ask that people remember to: maintain social distance, check into
+          the festival using the Check In Qld app, please do not attend if you
+          are unwell or have recently returned from overseas or a hotspot please
+          wear a mask If you have been in a restricted South East Queensland
+          area.
+        </p>
         <p>
           This year’s Festival presents a four day program of storytelling for
           all ages through all art forms. From music concerts backstage and
