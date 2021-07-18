1a6e43196956e4c13545491c78ae758bad4e2d07
mitchbrunke <65658409+mitchbrunke@users.noreply.github.com>
2021-07-19 08:43:35 +1000

    upgrade

--- components/FestivalProgram.js
+++ components/FestivalProgram.js
@@ -16,18 +16,17 @@ const FestivalProgram = () => {
           </p>
 
           <p>
-            Day 1 (July 1) begins at the renowned Maryborough Markets and
-            includes a Basket Weaving Workshop at Gatakers Artspace and ticketed
-            tours at Story Bank and Bond Store. 🧶 Day 2 (July 2) includes
-            ticketed tours at the unique Story Bank and Bond Store, along with
-            entry into Gatakers Artspace. 🎨 Day 3 (July 3) includes a Recycled
-            Materials Workshop at Gatakers Artspace, an incredible ticketed
-            performance 'Charlotte's Web' at the Brolga Theatre. Again tours are
-            available at Story Bank and the Bond Store. 🕸️🎭 Day 4 (July 4) ✨
-            DAY IN THE PARK ✨ highlights include the Chimney Sweep and Nanny
-            Race Challenge, Best Dressed, Picnic in the Park, various Book
-            Times, Trivia, Magic Workshop, art and music performances and much
-            more!
+            Day 1 begins at the renowned Maryborough Markets and includes a
+            Basket Weaving Workshop at Gatakers Artspace and ticketed tours at
+            Story Bank and Bond Store. 🧶 Day 2 includes ticketed tours at the
+            unique Story Bank and Bond Store, along with entry into Gatakers
+            Artspace. 🎨 Day 3 includes a Recycled Materials Workshop at
+            Gatakers Artspace, an incredible ticketed performance 'Charlotte's
+            Web' at the Brolga Theatre. Again tours are available at Story Bank
+            and the Bond Store. 🕸️🎭 Day 4 ✨ DAY IN THE PARK ✨ highlights
+            include the Chimney Sweep and Nanny Race Challenge, Best Dressed,
+            Picnic in the Park, various Book Times, Trivia, Magic Workshop, art
+            and music performances and much more!
           </p>
         </div>
       </div>
